cdec0a776a3b45c1fe18d15ecda63d4ce9b8f29f
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-05 08:55:28 +0700

    Update __main__.py

--- YMusic/__main__.py
+++ YMusic/__main__.py
@@ -4,21 +4,21 @@ from pytgcalls import idle
 
 from YMusic import LOGGER
 from YMusic.plugins import ALL_MODULES
-from YMusic import app, call
+from YMusic import YMusic, YMusicUser
 
 loop = asyncio.get_event_loop()
 
 
 async def init():
 
-    await app.start()
+    YMusicUser.start()
     LOGGER("YMusic").info("Account Started Successfully")
 
     for all_module in ALL_MODULES:
         importlib.import_module("YMusic.plugins" + all_module)
 
     LOGGER("YMusic.plugins").info("Successfully Imported Modules")
-    await call.start()
+    YMusic.start()
     await idle()
 
 

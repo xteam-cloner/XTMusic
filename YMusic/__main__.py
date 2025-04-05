f3790cf585190868d99c80a9603c4c89a50d3e40
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-05 09:12:21 +0700

    Update __main__.py

--- YMusic/__main__.py
+++ YMusic/__main__.py
@@ -1,7 +1,7 @@
 import importlib
 import asyncio
 from pytgcalls import idle
-
+from pytgcalls import PyTgCalls
 from YMusic import LOGGER
 from YMusic.plugins import ALL_MODULES
 from YMusic import YMusic, YMusicUser
@@ -18,9 +18,8 @@ async def init():
         importlib.import_module("YMusic.plugins" + all_module)
 
     LOGGER("YMusic.plugins").info("Successfully Imported Modules")
-    YMusic.start()
+    YMusicBot.start()
     await idle()
-
 
 if __name__ == "__main__":
     loop.run_until_complete(init())

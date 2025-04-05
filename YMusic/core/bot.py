ec2432044a8b072ae7059e1301142b208f4b82ae
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-04-05 09:10:38 +0700

    Update bot.py

--- YMusic/core/bot.py
+++ YMusic/core/bot.py
@@ -7,26 +7,20 @@ from ..logging import LOGGER
 class YMusicBot:
     def __init__(self):
         try:
-            self.YMusicUser = Client(
+            YMusicUser = Client(
                 name="AnonXAss1",
                 api_id=config.API_ID,
                 api_hash=config.API_HASH,
                 session_string=str(config.STRING1),
             )
-            self.one = PyTgCalls(
+            self.PyTgCalls(
                 self.YMusicUser,
                 cache_duration=100,
             )
+            
             LOGGER.info("YMusicBot berhasil diinisialisasi.")
         except Exception as e:
             LOGGER.error(f"Gagal menginisialisasi YMusicBot: {e}")
             raise  # Re-raise exception untuk penanganan lebih lanjut jika diperlukan
 
-YMusic = Client(
-    name="YMusic",
-    api_id=config.API_ID,
-    api_hash=config.API_HASH,
-    session_string=config.SESSION_STRING,
-)
-
-YMusicUser = PyTgCalls(YMusic)
+YMusicUser = PyTgCalls(YMusicBot)
